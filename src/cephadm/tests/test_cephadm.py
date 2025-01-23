ba451c0c56727057de50c243a9c17dacdd17cf80
Adam King <adking@redhat.com>
2025-01-23 13:58:33 -0500

    cephadm: add LimitCORE=infinity to ceph unit file template
    To stop from coredumpctl from truncating the core dumps
    which makes them much less useful
    
    Signed-off-by: Adam King <adking@redhat.com>
    (cherry picked from commit 7ad806935c7298d4eac384275e89bb81d312975d)
    
    Resolves: rhbz#2338598

--- src/cephadm/tests/test_cephadm.py
+++ src/cephadm/tests/test_cephadm.py
@@ -1864,6 +1864,7 @@ class TestIscsi:
             ctx.config_json = json.dumps(config_json)
             ctx.fsid = fsid
             _cephadm.get_parm.return_value = config_json
+            ctx.limit_core_infinity = False
 
             ident = _cephadm.DaemonIdentity(fsid, 'iscsi', 'daemon_id')
 
@@ -2414,6 +2415,7 @@ class TestSNMPGateway:
             ctx.fsid = fsid
             ctx.tcp_ports = '9464'
             _cephadm.get_parm.return_value = self.V2c_config
+            ctx.limit_core_infinity = False
 
             ident = _cephadm.DaemonIdentity(fsid, 'snmp-gateway', 'daemon_id')
             c = _cephadm.get_container(ctx, ident)
@@ -2443,6 +2445,7 @@ class TestSNMPGateway:
             ctx.fsid = fsid
             ctx.tcp_ports = '9465'
             _cephadm.get_parm.return_value = self.V3_no_priv_config
+            ctx.limit_core_infinity = False
 
             ident = _cephadm.DaemonIdentity(fsid, 'snmp-gateway', 'daemon_id')
             c = _cephadm.get_container(ctx, ident)
@@ -2472,6 +2475,7 @@ class TestSNMPGateway:
             ctx.fsid = fsid
             ctx.tcp_ports = '9464'
             _cephadm.get_parm.return_value = self.V3_priv_config
+            ctx.limit_core_infinity = False
 
             ident = _cephadm.DaemonIdentity(fsid, 'snmp-gateway', 'daemon_id')
             c = _cephadm.get_container(ctx, ident)
@@ -2501,6 +2505,7 @@ class TestSNMPGateway:
             ctx.fsid = fsid
             ctx.tcp_ports = '9464'
             _cephadm.get_parm.return_value = self.no_destination_config
+            ctx.limit_core_infinity = False
 
             with pytest.raises(Exception) as e:
                 c = _cephadm.get_container(
@@ -2517,6 +2522,7 @@ class TestSNMPGateway:
             ctx.fsid = fsid
             ctx.tcp_ports = '9464'
             _cephadm.get_parm.return_value = self.bad_version_config
+            ctx.limit_core_infinity = False
 
             with pytest.raises(Exception) as e:
                 c = _cephadm.get_container(
@@ -2692,6 +2698,7 @@ class TestJaeger:
             import json
             ctx.config_json = json.dumps(self.single_es_node_conf)
             ctx.fsid = fsid
+            ctx.limit_core_infinity = False
             ident = _cephadm.DaemonIdentity(fsid, 'jaeger-collector', 'daemon_id')
             c = _cephadm.get_container(ctx, ident)
             _cephadm.create_daemon_dirs(ctx, ident, 0, 0)
@@ -2712,6 +2719,7 @@ class TestJaeger:
             import json
             ctx.config_json = json.dumps(self.multiple_es_nodes_conf)
             ctx.fsid = fsid
+            ctx.limit_core_infinity = False
             ident = _cephadm.DaemonIdentity(fsid, 'jaeger-collector', 'daemon_id')
             c = _cephadm.get_container(ctx, ident)
             _cephadm.create_daemon_dirs(ctx, ident, 0, 0)
@@ -2732,6 +2740,7 @@ class TestJaeger:
             import json
             ctx.config_json = json.dumps(self.agent_conf)
             ctx.fsid = fsid
+            ctx.limit_core_infinity = False
             ident = _cephadm.DaemonIdentity(fsid, 'jaeger-agent', 'daemon_id')
             c = _cephadm.get_container(ctx, ident)
             _cephadm.create_daemon_dirs(ctx, ident, 0, 0)
